9c91a1ddd608f1dd310ad045623d49da3bb7d967
Mohamed Ayman Mohammedel-amin <40022241+kabantsh@users.noreply.github.com>
2020-03-16 08:45:20 +0200

    signature_script.sh
    Kabantsh signtaure script

---
+++ signature_script.sh
@@ -0,0 +1,23 @@
+echo 
+echo -e "whereis bash  | awk '{print $2}'"
+echo -e '# This script is been developed by kabantsh   '
+echo -e "#                               on `date +%F`   "
+echo -e '# for more information please contact with the email kabantsh@gmail.com   '
+echo -e '################   '
+echo -e '# Prerequisits:-   '
+echo -e '################   '
+echo 
+echo -e '#1- Operating System '
+echo -e '#-------------------'
+echo "#` termux-info | grep  -A1  "Android" | head -2`"
+echo 
+echo -e '#2- Bash 4version '
+echo -e '#------------------'
+echo "#$BASH_VERSION"
+echo 
+echo -e '################   '
+echo -e '# Description:-   '
+echo -e '################   '
+echo 
+e
+echo -e "\n#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
